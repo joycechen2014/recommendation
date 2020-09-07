a290bd52ccd5d74f20dbbc47d9d6114ef15234c7
Jinfeng Chen <jinfeng.chen@sjsu.edu>
2020-09-07 15:39:15 -0700

    first commit

---
+++ jupiter/src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -0,0 +1,13 @@
+package com.laioffer.job.db;
+
+public class MySQLDBUtil {
+    private static final String INSTANCE = "laiproject-instance2.cdznizk9ugkm.us-east-2.rds.amazonaws.com";
+    private static final String PORT_NUM = "3306";
+    public static final String DB_NAME = "laiproject";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "Cjf19860727!";
+    public static final String URL = "jdbc:mysql://"
+            + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
+            + "?user=" + USERNAME + "&password=" + PASSWORD
+            + "&autoReconnect=true&serverTimezone=UTC";
+}
