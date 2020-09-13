a3957de55046a2eb1d709536a8a3fef3ce5ceffa
joycechen2014 <48270532+joycechen2014@users.noreply.github.com>
2020-09-13 04:34:20 -0700

    Update MySQLDBUtil.java

--- jupiter/src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ jupiter/src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package com.laioffer.job.db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "laiproject-instance2.cdznizk9ugkm.us-east-2.rds.amazonaws.com";
-    private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = "laiproject";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "Cjf19860727!";
+    private static final String INSTANCE = "YOUR INSTANCE IP ADDRESS";
+    private static final String PORT_NUM = "PORT";
+    public static final String DB_NAME = "NAME";
+    private static final String USERNAME = "USERNAME";
+    private static final String PASSWORD = "PASSWORD";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
